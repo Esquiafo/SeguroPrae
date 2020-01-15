91c980ec522ee1bb818c926efd61be6db39ab9a0
Esquiafo <leocapone@hotmail.com>
2020-01-15 18:54:43 -0300

    cambio register

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -8,9 +8,35 @@ const userController = require('../controllers/userController')
 
 router.get('/register', userController.register);
 router.get('/login', userController.login);
+// router.post('/register', function(req, res) {
+//     Account.register(new Account({ username : req.body.username }), req.body.password, function(err, account) {
+//       if (err) {
+//        return res.render("register", {info: "Sorry. That username already exists. Try again."});
+//       }
+//   
+//       passport.authenticate('local')(req, res, function () {
+//         res.redirect('/');
+//       });
+//     });
+//   });
 
-// router.post("/register", function (req, res) {
-//   console.log('papita frita')
-// 		// res.send(fs.appendFileSync("pendientes.txt", "aprender node"))
-// 	  });
+router.post('/register', function (req, res) {
+    res.send(req.body)
+  })
+  
+  let diskStorage = multer.diskStorage({
+	destination: function (req, file, cb) {
+		cb(null, path.join(__dirname, '../data/user.json'));
+	},
+	filename: function (req, file, cb) {
+		let finalName = Date.now() + path.extname(file.originalname);
+		cb(null, finalName);
+	}
+});
+
+let upload = multer({ storage: diskStorage })
+
+
+
+module.exports = router;
 module.exports = router
