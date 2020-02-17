858ece8f0ae5e694ccb1501603f8442d08be5343
JaureguiCecilia <jaureguicecilia@yahoo.com.ar>
2020-02-17 16:43:59 -0300

    Sequilize v.6

--- src/controllers/productsController.js
+++ src/controllers/productsController.js
@@ -3,8 +3,8 @@ const Products = db.products;
 const PrdCategories = db.prdCategories;
 
 // Helper Functions
-function getAllProducts () {
-	let productsFileContent = fs.readFileSync(productsFilePath, {encoding:'utf-8'});
+function getAllProducts() {
+	let productsFileContent = fs.readFileSync(productsFilePath, { encoding: 'utf-8' });
 	let productsArray;
 	if (productsFileContent == '') {
 		productsArray = [];
@@ -14,18 +14,17 @@ function getAllProducts () {
 	return productsArray;
 }
 
-function guardarProductos (products) {
+function guardarProductos(products) {
 	fs.writeFileSync(productsFilePath, JSON.stringify(products, null, ' '));
 }
 
 module.exports = {
-     
+
+	// Falta
 	productCart: (req, res) => {
 		res.render('products/productCart');
 	},
 
-	
-	// PASADA
 	productDetailsroboperdida: (req, res) => {
 		Products
 			.findAll()
@@ -35,12 +34,11 @@ module.exports = {
 				});
 			})
 			.catch(error => res.send(error));
-	
+
 	},
 
-	// PASADA
 	productDetailsvida: (req, res) => {
-		
+
 		Products
 			.findAll()
 			.then(products => {
@@ -49,11 +47,11 @@ module.exports = {
 				});
 			})
 			.catch(error => res.send(error));
-			
+
 	},
 
-	// PASADA
 	productDetailsincendios: (req, res) => {
+
 		Products
 			.findAll()
 			.then(products => {
@@ -62,12 +60,11 @@ module.exports = {
 				});
 			})
 			.catch(error => res.send(error));
-		
-		},
-	
-	// PASADO
+
+	},
+
 	productAdd: (req, res) => {
-		
+
 		let prdCategories = PrdCategories.findAll();
 
 		Promise
@@ -80,11 +77,10 @@ module.exports = {
 			.catch(error => res.send(error));
 
 		return;
-		
+
 	},
-	
-	// PASADA
-    storeProduct: (req, res) => {		
+
+	storeProduct: (req, res) => {
 		console.log(req.body);
 		Products
 			.create(req.body)
@@ -93,8 +89,7 @@ module.exports = {
 			})
 			.catch(error => res.send(error));
 	},
-	
-	/* PASADA */
+
 	productsAll: (req, res) => {
 		Products
 			.findAll()
@@ -105,25 +100,49 @@ module.exports = {
 			})
 			.catch(error => res.send(error));
 	},
-	
+
 	deleteProduct: (req, res) => {
-		let productsData = getAllProducts();
-		finalPrdData = productsData.filter(function(product){
-			return product.id != req.params.productId;
-		});
-		guardarProductos(finalPrdData);
-		res.redirect('/products/allProducts');
+
+		Products
+			.findByPk(req.params.productId)
+			.then(product => {
+				product.destroy();
+				return res.redirect('/products/allProducts');
+			})
+			.catch(error => res.send(error));
+	},
+
+	updProductShow: (req, res) => {
+
+		Products
+			.findByPk(req.params.productId, {
+				include: ['prdCategory']
+			})
+			.then(product => {
+				return res.render('products/productUpd', { updPrd: product });
+			})
+			.catch(error => res.send(error));
+
 	},
 
 	updProduct: (req, res) => {
-		let productsData = getAllProducts();
-		updPrd = productsData.filter(function(product){
-			return product.id == req.params.productId;
-		});
-		
-		res.render('products/productUpd', {updPrd: updPrd});
+
+		Products
+			.update({
+				prdCategId: req.body.prdCategId,
+				description: req.body.description,
+				price: req.body.price
+			},
+				{
+					where: {
+						id: req.params.productId,
+					}
+				})
+			.catch(error => res.send(error));
+
+			return res.redirect('/products/allProducts');
 	},
-	
+
+
 }
 
- 
