948b2205fabb7282b5511d239c985530c9c2a67c
JaureguiCecilia <jaureguicecilia@yahoo.com.ar>
2020-04-15 03:00:04 -0300

    API productos paginado v2

--- src/controllers/api/productsController.js
+++ src/controllers/api/productsController.js
@@ -2,31 +2,71 @@ const db = require('../../database/models/');
 const Products = db.products;
 const PrdCategories = db.prdCategories;
 
-var express = require('express')
-var cookieParser = require('cookie-parser')
-
-var app = express()
-app.use(cookieParser())
-
 module.exports = {
 	productsAll: (req, res) => {
-		Products
-			.findAll()
-			.then(function(products) {
-                for (let i = 0; i < products.length; i++) {
-                    products[i].setDataValue('endpoint', 'api/products/' + products[i].id);
+        let offsetPage = 0;
+                
+        if (req.query.page){
+            if (Number(req.query.page) == 1){
+                offsetPage = 0;
+            }
+            offsetPage = (Number(req.query.page) - 1) * 2;
+        }
+
+        Products
+            .findAndCountAll({
+                attributes: ["id", "description", "price"],
+                include: [{ 
+                    association: 'prdCategory',
+                    attributes: ["id", "name"]
+                }],
+                limit: 2,
+                offset: offsetPage
+            })
+            .then( async products => {
+                let categories = await PrdCategories.findAll({
+                    include: [{ 
+                        association: 'products',
+                        attributes: ["id"]
+                    }]
+                });        
+            
+                let totalPages = Math.ceil(products.count/2);    
+                let productList = products.rows.map( oneProduct => {
+                    return oneProduct = {
+                        id: oneProduct.id,
+                        description: oneProduct.description,
+                        price: oneProduct.price,
+                        category: oneProduct.prdCategId,
+                        url: `http://localhost:3000/api/products/allProducts/${oneProduct.id}`,
+                        }
+                });
+
+                let page = Number(req.query.page);
+                let nextUrl = `http://localhost:3000/api/products/allProducts/?page=2`;
+                let prevUrl = null;
+                if(page && page != 1 ){
+                    
+                    prevUrl = `http://localhost:3000/api/products/allProducts/?page=${(page - 1)}`;
+                    
+                    if( page < totalPages ) {
+                        nextUrl = `http://localhost:3000/api/products/allProducts/?page=${page + 1}`;
+                    } else {
+                        nextUrl = null;
+                    }
+                    
                 }
 
-                let resp = {
-                    meta: {
-                        status: 200,
-                        total: products.length,
-                        url: '/api/products/allProducts'
-                    }, 
-                    data: products
-                };
+                return res.status(200).json({
+                    status: 200,
+                    total_results: products.count,
+                    total_categories: categories.length,
+                    next: nextUrl,
+                    prev: prevUrl,
+                    total_pages: totalPages,
+                    products: productList
+                });
                 
-                return res.json(resp);
 			})
 			.catch(error => res.send(error));
 	},
